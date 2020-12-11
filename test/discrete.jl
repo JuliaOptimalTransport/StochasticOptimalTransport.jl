25b1cab96c234ceb8e59c4d81b59e45e046dcd08
David Widmann <dev+git@devmotion.de>
2020-12-11 22:15:30 +0100

    Change tolerances

--- test/discrete.jl
+++ test/discrete.jl
@@ -8,9 +8,9 @@
         μ = SOT.DiscreteMeasure(xs, ps)
         ν = SOT.DiscreteMeasure(xs, ps)
 
-        @test SOT.wasserstein(c, μ, ν) ≈ 0 atol=1e-4
-        @test SOT.wasserstein(c, μ, ν, 1e-6) ≈ 0 atol=1e-4
-        @test SOT.wasserstein(c, μ, ν, 1e-3) ≈ 0 atol=1e-4
+        @test SOT.wasserstein(c, μ, ν; stepsize = 0.05) ≈ 0 atol=1e-4
+        @test SOT.wasserstein(c, μ, ν, 1e-6; stepsize = 0.05) ≈ 0 atol=1e-4
+        @test SOT.wasserstein(c, μ, ν, 1e-3; stepsize = 0.05) ≈ 0 atol=1e-4
     end
 
     @testset "uniform weights" begin
@@ -26,8 +26,8 @@
         # analytic Wasserstein distance
         d = sum(abs, sort(xs) .- sort(ys)) / n
 
-        @test SOT.wasserstein(c, μ, ν; stepsize = 0.05) ≈ d atol=1e-2
-        @test SOT.wasserstein(c, μ, ν, 1e-6;  stepsize = 0.05) ≈ d atol=1e-2
-        @test SOT.wasserstein(c, μ, ν, 1e-3; stepsize = 0.05) ≈ d atol=1e-2
+        @test SOT.wasserstein(c, μ, ν; stepsize = 0.05) ≈ d atol=5e-2
+        @test SOT.wasserstein(c, μ, ν, 1e-6;  stepsize = 0.05) ≈ d atol=5e-2
+        @test SOT.wasserstein(c, μ, ν, 1e-3; stepsize = 0.05) ≈ d atol=5e-2
     end
 end
